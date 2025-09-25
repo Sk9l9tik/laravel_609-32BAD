f24e5e3acbb90514a3a60338739c8e66da399be0
Sk9l9tik <adbelosheev@gmail.com>
2025-09-25 07:38:12 +0000

    migrations task

---
+++ database/migrations/2025_09_25_065813_add_props_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('paste_id');
+            $table->unsignedBigInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('paste_id');
+            $table->dropColumn('user_id');
+        });
+    }
+};
+
---
+++ database/migrations/2025_09_25_072516_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('paste_id');
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->foreign('paste_id')->references('id')->on('pastes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
